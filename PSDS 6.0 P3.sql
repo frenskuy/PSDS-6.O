e3048138788d093c71e96bf380974b26e4de82b5
frenskuy <116736412+frenskuy@users.noreply.github.com>
2023-11-29 13:24:11 +0700

    Add files via upload

---
+++ PSDS 6.0 P3.sql
@@ -0,0 +1,15 @@
+
+SELECT * FROM mahasiswamtk m2
+-- prefix jelas --
+SELECT mahasiswamtk.Nama FROM mahasiswamtk
+
+SELECT m.Nama , m.NIM FROM mahasiswamtk m
+
+-- select with prefix As
+
+-- Mengganti Nama kolom dari variabel "nama"
+SELECT nama AS Name FROM mahasiswamtk
+--  Menampilkan perubahan nama kolom
+SELECT nama AS Name, nim, asal from mahasiswamtk
+
+
